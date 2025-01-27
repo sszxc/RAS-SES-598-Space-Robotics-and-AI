33858e5ac9a1ca47075b000e9c7d12e1f2a81025
Xuechao <xzhan742@asu.edu>
2025-01-27 10:51:42 -0700

    initial adjustment of KD controller parameters

--- assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
+++ assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
@@ -19,11 +19,11 @@ class BoustrophedonController(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('Kp_linear', 10.0),
-                ('Kd_linear', 0.1),
-                ('Kp_angular', 5.0),
-                ('Kd_angular', 0.2),
-                ('spacing', 1.0)
+                ('Kp_linear', 10.0),  # 10.0
+                ('Kd_linear', 0.04),  # 0.1
+                ('Kp_angular', 9.0),  # 5.0
+                ('Kd_angular', 0.02),  # 0.2
+                ('spacing', 0.5)  # 1.0
             ]
         )
         
